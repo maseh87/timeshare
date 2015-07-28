a608f6969ceaa01d92dc19208aa92b0cd82078ec
Mason <mhargrove@popsugar.com>
2015-07-28 14:01:42 -0700

    added fb authentication

---
+++ client/app/app.js
@@ -0,0 +1,22 @@
+var ref = new Firebase("https://timeshare.firebaseio.com");
+var users = ref.child('users');
+
+var fbLogin = function() {
+  ref.authWithOAuthPopup("facebook", function(error, authData) {
+    if (error) {
+      console.log("Login Failed!", error);
+    } else {
+      console.log("Authenticated successfully with payload:", authData);
+      users.set({
+        name: authData.facebook.displayName,
+        id: authData.facebook.id
+      });
+    }
+  });
+}
+
+
+ref.on('child_added', function(snapshot, prevChildKey) {
+  var newUser = snapshot.val()
+  console.log('a new user named ' + newUser.name + ' was created');
+});
