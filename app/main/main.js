ac84819548aecae012d38b57fcf3410fd7a48c41
Greg Domorski <greg.domorski@gmail.com>
2015-07-30 00:35:06 -0400

    Minor Updates

--- app/main/main.js
+++ app/main/main.js
@@ -1,52 +1,34 @@
-/*!
- *
- *  Web Starter Kit
- *  Copyright 2014 Google Inc. All rights reserved.
- *
- *  Licensed under the Apache License, Version 2.0 (the "License");
- *  you may not use this file except in compliance with the License.
- *  You may obtain a copy of the License at
- *
- *    https://www.apache.org/licenses/LICENSE-2.0
- *
- *  Unless required by applicable law or agreed to in writing, software
- *  distributed under the License is distributed on an "AS IS" BASIS,
- *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *  See the License for the specific language governing permissions and
- *  limitations under the License
- *
- */
-(function () {
-  'use strict';
 
-  console.log("mason");
-  
-  var querySelector = document.querySelector.bind(document);
 
-  var navdrawerContainer = querySelector('.navdrawer-container');
-  var body = document.body;
-  var appbarElement = querySelector('.app-bar');
-  var menuBtn = querySelector('.menu');
-  var main = querySelector('main');
 
-  function closeMenu() {
-    body.classList.remove('open');
-    appbarElement.classList.remove('open');
-    navdrawerContainer.classList.remove('open');
-  }
 
-  function toggleMenu() {
-    body.classList.toggle('open');
-    appbarElement.classList.toggle('open');
-    navdrawerContainer.classList.toggle('open');
-    navdrawerContainer.classList.add('opened');
-  }
 
-  main.addEventListener('click', closeMenu);
-  menuBtn.addEventListener('click', toggleMenu);
-  navdrawerContainer.addEventListener('click', function (event) {
-    if (event.target.nodeName === 'A' || event.target.nodeName === 'LI') {
-      closeMenu();
-    }
-  });
-})();
+
+
+
+ var ref = new Firebase("https://timeshare.firebaseio.com");
+ var users = ref.child('users');
+ 
+function fbLogin() {
+   ref.authWithOAuthPopup("facebook", function(error, authData) {
+     if (error) {
+       console.log("Login Failed!", error);
+     } else {
+       console.log("Authenticated successfully with payload:", authData);
+       users.set({
+         name: authData.facebook.displayName,
+         id: authData.facebook.id
+       });
+     }
+   });
+ }
+
+
+
+ ref.on('child_added', function(snapshot, prevChildKey) {
+   var newUser = snapshot.val()
+   console.log('a new user named ' + newUser.name + ' was created');
+ });
+ 
+
+
